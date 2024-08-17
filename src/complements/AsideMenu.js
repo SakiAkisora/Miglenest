862708699d9253e264024f7749b58a27555c80b4
Saki Akisora <akisorasakich@gmail.com>
2024-08-16 22:26:37 -0600

    Modificacion del menu lateral

--- src/complements/AsideMenu.js
+++ src/complements/AsideMenu.js
@@ -1,61 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import home from '../assets/image/casa.png';
-import category from '../assets/image/etiqueta.png'
-import saved from '../assets/image/estrella.png'
-import likes from '../assets/image/corazon.png'
-import settings from '../assets/image/configuraciones.png'
-import info from '../assets/image/lentes.png'
+import category from '../assets/image/etiqueta.png';
+import saved from '../assets/image/estrella.png';
+import likes from '../assets/image/corazon.png';
+import settings from '../assets/image/configuraciones.png';
+import info from '../assets/image/lentes.png';
 import '../assets/styles/asideMenu.css';
 
 export const AsideMenu = ({ isMenuActive }) => {
     const [activeItem, setActiveItem] = useState('home');
+
     const handleIconClick = (item) => {
         setActiveItem(item);
     };
 
     return (
         <div className={`containerAside ${isMenuActive ? 'active' : ''}`}>
-            <div className={`icons`}>
+            <div className="icons">
                 <div
-                    className={`icon-item home ${activeItem === 'home' ? 'activeHome' : ''}`}
+                    className={`icon-item ${activeItem === 'home' ? 'active' : ''}`}
                     onClick={() => handleIconClick('home')}
                 >
-                    <img src={home} alt='Icono de inicio' />
+                    <img src={home} alt="Icono de inicio" />
                     {isMenuActive && <span>Inicio</span>}
                 </div>
                 <div
-                    className={`icon-item category ${activeItem === 'category' ? 'activeCategory' : ''}`}
+                    className={`icon-item ${activeItem === 'category' ? 'active' : ''}`}
                     onClick={() => handleIconClick('category')}
                 >
-                    <img src={category} alt='Icono de categoría' />
+                    <img src={category} alt="Icono de categoría" />
                     {isMenuActive && <span>Categorías</span>}
                 </div>
                 <div
-                    className={`icon-item saved ${activeItem === 'saved' ? 'activeSaved' : ''}`}
+                    className={`icon-item ${activeItem === 'saved' ? 'active' : ''}`}
                     onClick={() => handleIconClick('saved')}
                 >
-                    <img src={saved} alt='Icono de guardados' />
+                    <img src={saved} alt="Icono de guardados" />
                     {isMenuActive && <span>Guardados</span>}
                 </div>
                 <div
-                    className={`icon-item likes ${activeItem === 'likes' ? 'activeLikes' : ''}`}
+                    className={`icon-item ${activeItem === 'likes' ? 'active' : ''}`}
                     onClick={() => handleIconClick('likes')}
                 >
-                    <img src={likes} alt='Icono de me gusta' />
+                    <img src={likes} alt="Icono de me gusta" />
                     {isMenuActive && <span>Tus me gusta</span>}
                 </div>
                 <div
-                    className={`icon-item settings ${activeItem === 'settings' ? 'activeSettings' : ''}`}
+                    className={`icon-item ${activeItem === 'settings' ? 'active' : ''}`}
                     onClick={() => handleIconClick('settings')}
                 >
-                    <img src={settings} alt='Icono de configuraciones' />
+                    <img src={settings} alt="Icono de configuraciones" />
                     {isMenuActive && <span>Configuración</span>}
                 </div>
                 <div
-                    className={`icon-item info ${activeItem === 'info' ? 'activeInfo' : ''}`}
+                    className={`icon-item ${activeItem === 'info' ? 'active' : ''}`}
                     onClick={() => handleIconClick('info')}
                 >
-                    <img src={info} alt='Icono de información' />
+                    <img src={info} alt="Icono de información" />
                     {isMenuActive && <span>Información</span>}
                 </div>
             </div>
